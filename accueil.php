b5d6c0ba9eb92cc2faa4aa5dad8267ec706ea03b
rambo53 <71452106+rambo53@users.noreply.github.com>
2021-10-08 08:53:08 +0200

    Update accueil.php

--- accueil.php
+++ accueil.php
@@ -14,22 +14,11 @@
 
 
     <div class="container site">
-        <h1 class="text-logo"></span>BURGER CODE</span></h1>
+    <?php
+      require_once'navbar.php';
 
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <div class="container-fluid">             
-              <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div class="navbar-nav">
-                  <a class="nav-link active" data-toggle="tab" href="#1">Menu</a>
-                  <a class="nav-link" href="#2" data-toggle="tab">Burgers</a>
-                  <a class="nav-link" href="#3" data-toggle="tab">Snacks</a>
-                  <a class="nav-link" href="#4" data-toggle="tab">Salades</a>
-                  <a class="nav-link" href="#5" data-toggle="tab">Boissons</a>
-                  <a class="nav-link" href="#6" data-toggle="tab">Desserts</a>
-                </div>
-              </div>
-            </div>
-          </nav>
+      getNavbar();
+    ?>
       
       <div class="tab-content mt-2">
 
